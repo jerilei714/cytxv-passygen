b9042ad05c33713651543da201ec6511a8317050
JeriCodes1224 <jeri_concepcion@dlsu.edu.ph>
2023-11-30 05:34:07 +0800

    replaced random with secret module

--- cytxv.py
+++ cytxv.py
@@ -1,17 +1,17 @@
 import argparse
 import hashlib
-import random
+import secrets
 import string
 import os
 
 def generate_password(lower, upper, digits, special):
-    password = ''.join(random.choice(string.ascii_lowercase) for _ in range(lower))
-    password += ''.join(random.choice(string.ascii_uppercase) for _ in range(upper))
-    password += ''.join(random.choice(string.digits) for _ in range(digits))
-    password += ''.join(random.choice(string.punctuation) for _ in range(special))
+    password = ''.join(secrets.choice(string.ascii_lowercase) for _ in range(lower))
+    password += ''.join(secrets.choice(string.ascii_uppercase) for _ in range(upper))
+    password += ''.join(secrets.choice(string.digits) for _ in range(digits))
+    password += ''.join(secrets.choice(string.punctuation) for _ in range(special))
 
     password_list = list(password)
-    random.shuffle(password_list)
+    secrets.SystemRandom().shuffle(password_list)
     password = ''.join(password_list)
 
     return password
@@ -36,17 +36,20 @@ def main():
     """)
     print("1. Fast Generate")
     print("2. Personalized Generate")
-    choice = int(input("Enter your choice: "))
-
-    if choice == 1:
-        lower = upper = digits = special = 4  # Predefined configuration for fast generation
-    elif choice == 2:
-        lower = int(input("Enter the number of lowercase characters: "))
-        upper = int(input("Enter the number of uppercase characters: "))
-        digits = int(input("Enter the number of digits: "))
-        special = int(input("Enter the number of special characters: "))
-    else:
-        print("Invalid choice!")
+    try:
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            lower = upper = digits = special = 4  # Predefined configuration for fast generation
+        elif choice == 2:
+            lower = int(input("Enter the number of lowercase characters: "))
+            upper = int(input("Enter the number of uppercase characters: "))
+            digits = int(input("Enter the number of digits: "))
+            special = int(input("Enter the number of special characters: "))
+        else:
+            print("Invalid choice!")
+            return
+    except ValueError:
+        print("Invalid input! Please enter a number.")
         return
 
     password = generate_password(lower, upper, digits, special)
@@ -54,11 +57,10 @@ def main():
 
     print(f"Password: {password}")
     print(f"Hashed Password: {hashed_password}")
-    
+
 def clear_command_line():
-        os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     clear_command_line()
     main()
-
