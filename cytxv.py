9ebb67c8baae1f016adb701f8a6ee9099ce170f8
JeriCodes1224 <jeri_concepcion@dlsu.edu.ph>
2023-11-30 04:47:19 +0800

    Added Fast Generate Option and.. ASCII ART LOL

--- cytxv.py
+++ cytxv.py
@@ -1,15 +1,14 @@
+import argparse
 import hashlib
 import random
 import string
 
 def generate_password(lower, upper, digits, special):
-    # Generate a random password
     password = ''.join(random.choice(string.ascii_lowercase) for _ in range(lower))
     password += ''.join(random.choice(string.ascii_uppercase) for _ in range(upper))
     password += ''.join(random.choice(string.digits) for _ in range(digits))
     password += ''.join(random.choice(string.punctuation) for _ in range(special))
 
-    # Convert the password into a list and shuffle it
     password_list = list(password)
     random.shuffle(password_list)
     password = ''.join(password_list)
@@ -17,18 +16,43 @@ def generate_password(lower, upper, digits, special):
     return password
 
 def hash_password(password):
-    # Hash the password using SHA256
     sha_signature = hashlib.sha256(password.encode()).hexdigest()
     return sha_signature
 
-# Ask the user to specify the number of each type of character
-lower = int(input("Enter the number of lowercase characters: "))
-upper = int(input("Enter the number of uppercase characters: "))
-digits = int(input("Enter the number of digits: "))
-special = int(input("Enter the number of special characters: "))
+def main():
+    print("===================================")
+    print("  WELCOME TO CYTXV PASSYGEN")
+    print("===================================")
+    print("""
+     _____
+    |  __ \\
+    | |__) |__  _ __  _ __   ___ _ __
+    |  ___/ _ \\| '_ \\| '_ \\ / _ \\ '__|
+    | |  | (_) | |_) | |_) |  __/ |
+    |_|   \\___/| .__/| .__/ \\___|_|
+               | |   | |
+               |_|   |_|
+    """)
+    print("1. Fast Generate")
+    print("2. Personalized Generate")
+    choice = int(input("Enter your choice: "))
 
-password = generate_password(lower, upper, digits, special)
-hashed_password = hash_password(password)
+    if choice == 1:
+        lower = upper = digits = special = 4  # Predefined configuration for fast generation
+    elif choice == 2:
+        lower = int(input("Enter the number of lowercase characters: "))
+        upper = int(input("Enter the number of uppercase characters: "))
+        digits = int(input("Enter the number of digits: "))
+        special = int(input("Enter the number of special characters: "))
+    else:
+        print("[red]Invalid choice![/red]")
+        return
 
-print(f"Password: {password}")
-print(f"Hashed Password: {hashed_password}")
+    password = generate_password(lower, upper, digits, special)
+    hashed_password = hash_password(password)
+
+    print(f"[green]Password: {password}")
+    print(f"[red]Hashed Password: {hashed_password}")
+
+if __name__ == "__main__":
+    main()
